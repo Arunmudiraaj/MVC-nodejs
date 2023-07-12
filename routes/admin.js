c4d25a7a04293fc567c9f9e65975628d0a3e7895
Arunmudiraaj <arunmudiraj18@gmail.com>
2023-07-12 19:34:02 +0530

    dynamic routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
-const express = require("express");
+const path = require('path');
 
-const productsControllers = require("../controllers/products");
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", productsControllers.getAddProductPage);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", productsControllers.addProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
