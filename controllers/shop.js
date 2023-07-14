352b995ad176dad4444fb088e7c28c914b584d41
Arunmudiraaj <arunmudiraj18@gmail.com>
2023-07-14 20:09:09 +0530

    connected mysql to node app

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,7 +13,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
-    res.redirect("shop/product-detail", {
+    res.render("shop/product-detail", {
       product: product,
       pageTitle: product.title,
       path: "/products",
@@ -36,6 +36,11 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
